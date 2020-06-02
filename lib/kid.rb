619534aa2fa5fe0b68caf06e7827626c0aba5ec2
Jonah Mallard <benjaminjmallard@gmail.com>
2020-06-02 20:25:21 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,20 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance
+  include FancyDance::InstanceMethods
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+   extend MetaDancing
+   
+   extend FancyDance::ClassMethods
+  
+  
+   
+end
